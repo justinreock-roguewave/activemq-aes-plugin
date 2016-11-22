5f02af26023e282b02843c7e21ae529e9feae38a
justinreock <justin.reock@openlogic.com>
2016-11-22 13:10:35 -0500

    Update with dummy decrypt on messageConsume

--- src/main/java/com/roguewave/oss/activemq/ActiveMQPGPBroker.java
+++ src/main/java/com/roguewave/oss/activemq/ActiveMQPGPBroker.java
@@ -7,6 +7,8 @@ import org.apache.activemq.broker.BrokerFilter;
 import org.apache.activemq.broker.ProducerBrokerExchange;
 import org.apache.activemq.command.Message;
 import org.apache.activemq.command.ActiveMQTextMessage;
+import org.apache.activemq.broker.ConnectionContext;
+import org.apache.activemq.broker.region.MessageReference;
 
 public class ActiveMQPGPBroker extends BrokerFilter {
 
@@ -18,8 +20,7 @@ public class ActiveMQPGPBroker extends BrokerFilter {
 	public Message encryptMessage(Message mesg) {
 		
 		String mesgBody = "";
-		ActiveMQTextMessage tm = (ActiveMQTextMessage) mesg.getMessage();
-		tm.setReadOnlyBody(false);
+		ActiveMQTextMessage tm = initializeTextMessage(mesg);
 		try {
 			mesgBody = tm.getText();
 		}
@@ -42,10 +43,53 @@ public class ActiveMQPGPBroker extends BrokerFilter {
 		return tm;
 		
 	}
+	
+	public Message decryptMessage(Message mesg) {
+		
+		String mesgBody = "";
+		ActiveMQTextMessage tm = initializeTextMessage(mesg);
+		try {
+			mesgBody = tm.getText();
+		}
+		catch (JMSException e) {
+			System.out.println("Error:  Could not get message body contents");
+		    return mesg;
+		}
+		
+		mesgBody = mesgBody.replace("ENCRYPTED: ", "DECRYPTED: ");
+		mesgBody = "D: " + mesgBody;
+		
+		try {
+		    tm.setText(mesgBody);
+		}
+		catch (Exception e) {
+			System.out.println("Error: Could not write to message body");
+			System.out.println(e.getMessage());
+			return mesg;
+		}
+		
+		return tm;
+				
+	}
+	
+	public ActiveMQTextMessage initializeTextMessage(Message mesg) {
+		
+		ActiveMQTextMessage tm = (ActiveMQTextMessage) mesg.getMessage();
+		tm.setReadOnlyBody(false);
+		return tm;
+		
+	}
 
 	public void send(ProducerBrokerExchange producerExchange, Message messageSend) throws Exception {
 		ActiveMQTextMessage encryptedMessage = (ActiveMQTextMessage) encryptMessage(messageSend.getMessage());
 		next.send(producerExchange, encryptedMessage);
+	}
+	
+	public void messageConsumed(ConnectionContext context, MessageReference messageReference) {
+		ActiveMQTextMessage encryptedMessage = (ActiveMQTextMessage) messageReference.getMessage();
+		ActiveMQTextMessage decryptedMessage = (ActiveMQTextMessage) decryptMessage(encryptedMessage);
+		next.messageConsumed(context, decryptedMessage);
+		
 	}
 	
 }
